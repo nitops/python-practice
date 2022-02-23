141e3d42cc8c63b11ff560efd2ab668c798b5a90
njain51 <official.nitinjain@gmail.com>
2022-02-22 22:16:28 -0500

    basic python class variable example

---
+++ 1_basic_pyhon/1_string_examples/1.2_fstring_example.py
@@ -0,0 +1,7 @@
+""" fString demo here"""
+
+var = "Geeks"
+print(f"{var}for{var} is a portal for {var}")
+
+
+
---
+++ 3_python_examples/5_decorator_examples/1/app.py
@@ -0,0 +1,21 @@
+""" An example on decorator which stores log output to file and also print to screen"""
+
+
+def logged(function):
+    def wrapper(*args, **kwargs):
+        value = function(*args, **kwargs)
+        with open('logfile.txt', 'a+') as logfile:
+            fname = function.__name__
+            print(f"{fname} returned value {value}")
+            logfile.write(f"{fname} returned value {value} \n")
+        return value
+    return wrapper
+
+
+@logged
+def add(x, y):
+    return x + y
+
+
+print(add(4, 6))  # this will call decorator which will add logs to logfile.txt and also print to screen
+
