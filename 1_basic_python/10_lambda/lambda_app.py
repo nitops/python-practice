3942bb0cb0e082e4910d6e71bbc3cc2c01614602
njain51 <official.nitinjain@gmail.com>
2023-02-23 20:53:09 -0500

    basic python examples

---
+++ 1_basic_python/10_lambda/lambda_app.py
@@ -0,0 +1,5 @@
+b = lambda a: a + 10
+print(b(2))
+
+z = lambda x, y: x * y
+print(z(4, 3))
---
+++ 1_basic_python/11_dictionaries/11_app1.py
@@ -0,0 +1,11 @@
+# define a dictionary
+animals = {"cat": "carnivorous mammal",
+           "dog": "furry canine"}
+
+# print value for cat
+print(animals["cat"])
+
+# add another value
+
+animals["parrot"] = "talking bird"
+print(animals)
